226a422aa8a6fcf71e53eb68a3c561ad3ff66b16
Mahy02 <Mahinouralaa2002@gmail.com>
2023-06-26 14:16:47 +0300

    Models And Admin

--- django/MyPortfolio/admin.py
+++ django/MyPortfolio/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Home, About, Profile, Project, Internships, Achievement, StudentActivity, Category, Skills, Technology
+
+# Home
+admin.site.register(Home)
+
+
+# About
+class ProfileInLine(admin.TabularInline):
+    model= Profile
+    extra=1
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    inlines =[
+        ProfileInLine,
+    ]
+
+
+# Skills
+class SkillsInLine(admin.TabularInline):
+    model= Skills
+    extra=2
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines=[
+        SkillsInLine,
+    ]
+
+# Achievement
+admin.site.register(Achievement)
+
+# StudentActivity
+admin.site.register(StudentActivity)
+
+# Internships
+class ITechnologyInline(admin.TabularInline):
+    model = Internships.tools_and_technologies.through
+    extra = 1
+
+@admin.register(Internships)
+class InternshipsAdmin(admin.ModelAdmin):
+    inlines = [ITechnologyInline]
+
+# Project
+class PTechnologyInline(admin.TabularInline):
+    model = Project.tools_and_technologies.through
+    extra = 1
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [PTechnologyInline]
+    
--- django/MyPortfolio/models.py
+++ django/MyPortfolio/models.py
@@ -1,4 +1,130 @@
 from django.db import models
 
 # Create your models here.
+ 
 
+
+# HOME SECTION
+
+class Home(models.Model):
+    name= models.CharField(max_length=100)
+    greetings_1 = models.CharField(max_length=10)
+    greetings_2 = models.CharField(max_length=10)
+    picture = models.ImageField(upload_to = 'picture/')
+    #save time when modified
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+      return self.name
+    
+
+
+# ABOUT SECTION
+
+class About(models.Model):
+   heading = models.CharField(max_length=200)
+   career= models.CharField(max_length=100)
+   description= models.TextField(blank=False)
+   profile_img= models.ImageField(upload_to='profile/')
+
+   updated= models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+      return self.career
+   
+class Profile(models.Model):
+   about = models.ForeignKey(About, on_delete=models.CASCADE)
+   social_name= models.CharField(max_length=10)
+   link = models.URLField(max_length=200)
+
+
+
+# SKILLS SECTION
+
+class Category(models.Model):
+   name= models.CharField(max_length=20)
+   
+   updated= models.DateTimeField(auto_now=True)
+
+   class Meta:
+      verbose_name= 'Skill'
+      verbose_name_plural= 'Skills'
+
+   def __str__(self):
+        return self.name
+   
+   
+class Skills(models.Model):
+   category= models.ForeignKey(Category, on_delete=models.CASCADE)
+   skill_name= models.CharField(max_length=50)
+
+
+
+# INTERNSHIPS SECTION
+#company name, role, from, to, description, Technologies used, image1, image2
+
+class Technology(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Internships(models.Model):
+    company_name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(blank=False)
+    tools_and_technologies = models.ManyToManyField(Technology)   #many internships can have many technologies and one technology can be in many internships
+    image1 = models.ImageField(upload_to='internships/')
+    image2 = models.ImageField(upload_to='internships/', null=True, blank=True)
+
+    def __str__(self):
+        return self.company_name
+    
+
+# PROJECTS SECTION
+#project name, year, projecttype, description, tools and technologies
+    
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    year = models.PositiveIntegerField()
+    project_type = models.CharField(max_length=500)
+    description = models.TextField(blank=False)
+    tools_and_technologies = models.ManyToManyField(Technology)
+
+    def __str__(self):
+        return self.project_name
+
+
+
+# STUDENT ACTIVITIES 
+#name,role, from, to, description, image1, image2
+
+class StudentActivity(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(blank=False)
+    image1 = models.ImageField(upload_to='student_activities/')
+    image2 = models.ImageField(upload_to='student_activities/')
+
+    def __str__(self):
+        return self.name
+    
+
+
+# ACHIEVEMNTS
+#name, year, description
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+    
